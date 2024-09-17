f3318d9a42d6d9bf72cc2ccfd96072f4ebdf418e
Albano <albigoal@outlook.com>
2024-09-17 13:49:18 +0200

    db 1.0

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Coding-in-C
-Exploring C
+sqlite
+
+SQLite version 3.41.2 2023-03-22 11:56:21
+Enter ".help" for usage hints.
+Connected to a transient in-memory database.
+Use ".open FILENAME" to reopen on a persistent database.
+
+
+sqlite> create table users (id int, username varchar(255), email varchar(255));
+sqlite> .tables
+users
+sqlite> .exit
--- main.c
+++ main.c
@@ -1,26 +1,55 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+typedef struct {
+  char* buffer;
+  size_t buffer_length;
+  ssize_t input_length;
+} InputBuffer;
 
-int main (){
-    printf ("welcome,Ayanokoji\n here are your infos \t password: HIF \n");
-    short i = 1.5;
-    int x = 2;
-    long y = 4;
-    float z = 4.9;
-    double a = 67676.28782;
-    long double b = 884728927498.290219721;
-    int number1;
-    int number2;
-    printf("go with a number1:\t");
-    scanf("%d",&number1);
+InputBuffer* new_input_buffer() {
+  InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+  input_buffer->buffer = NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
 
-    printf("go with a number2:\t");
-    scanf("%d",&number2);
-    
-    int f = number1*number2;
+  return input_buffer;
+}
 
-    printf ("%d * %d = %d",number1,number2,f);
+void print_prompt() { printf("db > "); }
 
-    return 0 ;
+void read_input(InputBuffer* input_buffer) {
+  ssize_t bytes_read =
+      getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+  if (bytes_read <= 0) {
+    printf("Error reading input\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Ignore trailing newline
+  input_buffer->input_length = bytes_read - 1;
+  input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+int main(int argc, char* argv[]) {
+  InputBuffer* input_buffer = new_input_buffer();
+  while (true) {
+    print_prompt();
+    read_input(input_buffer);
+
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+      close_input_buffer(input_buffer);
+      exit(EXIT_SUCCESS);
+    } else {
+      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+    }
+  }
 }
