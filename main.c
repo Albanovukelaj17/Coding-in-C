5ed79bf79d88776b950b14ceb95a677a1606f1d9
Albano <albigoal@outlook.com>
2024-09-17 11:42:15 +0200

    scan

--- main.c
+++ main.c
@@ -3,6 +3,24 @@
 
 
 int main (){
-    printf ("welcome,Ayanokoji\n here are your infos \t password: HIF");
+    printf ("welcome,Ayanokoji\n here are your infos \t password: HIF \n");
+    short i = 1.5;
+    int x = 2;
+    long y = 4;
+    float z = 4.9;
+    double a = 67676.28782;
+    long double b = 884728927498.290219721;
+    int number1;
+    int number2;
+    printf("go with a number1:\t");
+    scanf("%d",&number1);
+
+    printf("go with a number2:\t");
+    scanf("%d",&number2);
+    
+    int f = number1*number2;
+
+    printf ("%d * %d = %d",number1,number2,f);
+
     return 0 ;
 }
