bb067253b57edb3a50acb8b17aa12e89e1a6086a
Albano <albigoal@outlook.com>
2024-09-27 18:49:37 +0200

    end 1.2

--- main.c
+++ main.c
@@ -661,7 +661,7 @@ void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value) {
   */
 
   void* old_node = get_page(cursor->table->pager, cursor->page_num);
-  uint32_t old_max = get_node_max_key(old_node);
+  uint32_t old_max = get_node_max_key(cursor->table->pager, old_node); 
   uint32_t new_page_num = get_unused_page_num(cursor->table->pager);
   void* new_node = get_page(cursor->table->pager, new_page_num);
   initialize_leaf_node(new_node);
@@ -703,7 +703,7 @@ void leaf_node_split_and_insert(Cursor* cursor, uint32_t key, Row* value) {
     create_new_root(cursor->table, new_page_num);
   } else {
     uint32_t parent_page_num = *node_parent(old_node);
-    uint32_t new_max = get_node_max_key(old_node);
+    uint32_t new_max = get_node_max_key(cursor->table->pager,old_node);
     void* parent = get_page(cursor->table->pager, parent_page_num);
 
     update_internal_node_key(parent, old_max, new_max);
