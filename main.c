52242e9c7f2196ee12aabac8727f569f2d57dcc9
Albano <albigoal@outlook.com>
2024-09-27 13:04:03 +0200

    end 1.1

--- main.c
+++ main.c
@@ -428,26 +428,7 @@ Pager* pager_open(const char* filename) {
 
 
 
-void print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level) {
-       num_keys = *internal_node_num_keys(node);
-       indent(indentation_level);
-       printf("- internal (size %d)\n", num_keys);
 
-      if (num_keys > 0) {
-        for (uint32_t i = 0; i < num_keys; i++) {
-          child = *internal_node_child(node, i);
-          print_tree(pager, child, indentation_level + 1);
-
-          indent(indentation_level + 1);
-          printf("- key %d\n", *internal_node_key(node, i));
-        }
-        child = *internal_node_right_child(node);
-         print_tree(pager, child, indentation_level + 1);
-
-       }
-       break;
-   }
- 
 
 Table* db_open(const char* filename) {
     Pager* pager = pager_open(filename);
@@ -561,6 +542,48 @@ uint32_t internal_node_find_child(void* node, uint32_t key) {
   }
    return min_index;
 }
+
+void indent(uint32_t level) {
+  for (uint32_t i = 0; i < level; i++) {
+    printf("  ");
+  }
+}
+
+void print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level) {
+  void* node = get_page(pager, page_num);
+  uint32_t num_keys, child;
+
+  switch (get_node_type(node)) {
+    case (NODE_LEAF):
+      num_keys = *leaf_node_num_cells(node);
+      indent(indentation_level);
+      printf("- leaf (size %d)\n", num_keys);
+      for (uint32_t i = 0; i < num_keys; i++) {
+        indent(indentation_level + 1);
+        printf("- %d\n", *leaf_node_key(node, i));
+      }
+      break;
+   case (NODE_INTERNAL):
+      num_keys = *internal_node_num_keys(node);
+      indent(indentation_level);
+      printf("- internal (size %d)\n", num_keys);
+       if (num_keys > 0) {
+        for (uint32_t i = 0; i < num_keys; i++) {
+          child = *internal_node_child(node, i);
+          print_tree(pager, child, indentation_level + 1);
+
+          indent(indentation_level + 1);
+          printf("- key %d\n", *internal_node_key(node, i));
+        }
+        child = *internal_node_right_child(node);
+        print_tree(pager, child, indentation_level + 1);
+
+      }
+    
+      break;
+  }
+}
+
 
 
 void update_internal_node_key(void* node, uint32_t old_key, uint32_t new_key) {
