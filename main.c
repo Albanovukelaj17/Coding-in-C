052c6291fe26e76f6c4eae737e907d50829cf817
Albano <albigoal@outlook.com>
2024-09-28 15:10:49 +0200

    end 1.4 , 11/13tests

--- main.c
+++ main.c
@@ -905,14 +905,18 @@ void print_constants() {
 }
 
 
-MetaCommandResult do_meta_command(InputBuffer* input_buffer,Table* table) {
+MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
   if (strcmp(input_buffer->buffer, ".exit") == 0) {
-     close_input_buffer(input_buffer);
-     db_close(table);
+    close_input_buffer(input_buffer);
+    db_close(table);
     exit(EXIT_SUCCESS);
-     } else if (strcmp(input_buffer->buffer, ".constants") == 0) {
+  } else if (strcmp(input_buffer->buffer, ".constants") == 0) {
     printf("Constants:\n");
     print_constants();
+    return META_COMMAND_SUCCESS;
+  } else if (strcmp(input_buffer->buffer, ".btree") == 0) {
+    printf("Tree:\n");
+    print_tree(table->pager, table->root_page_num, 0);
     return META_COMMAND_SUCCESS;
   } else {
     return META_COMMAND_UNRECOGNIZED_COMMAND;
